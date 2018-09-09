07ad00a28d987e92c34d2ea8d52572077ea6312e
Sixuan Wu <wusixuan2@gmail.com>
2018-09-09 01:25:06 +0000

    a very basic version with simplest AI

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 function instructionMsg() {
     alert("Each player then takes turns picking a tile on the opposing player's grid, taking a shot at that tile. If the tile contains a ship, the shot is a HIT. If the tile does not contain a ship, the shot is a MISS. A ship is sunk if all the tiles for that ship have been marked as a HIT. The game ends when one player has sunk all of the opposing players ships.");
 }
-function createTenByTenBoard(num) {
+
+function createTenByTenBoard(num, tittle) {
   var rowOpen = "<tr>\n";
   var rowClose = "</tr>\n";
   var cellOpen = "<td class='cell' id='";
   var cellClose = "'></td>\n";
-  var tableContent = `<table class='table${num}'>\n`; //
+  var tableContent = `<table class='table${num}' id='table${num}'>\n`;
+  tableContent += tittle;
   for(var i = 1; i < 11; i++) {
     tableContent += rowOpen;
     for(var j = 0; j < 10; j++) {
       var row = cellOpen;
-      row += i;
       var letter = String.fromCharCode(65+j);
       row += letter;
+      row += i;
       row += cellClose;
       tableContent += row;
     }
     tableContent += rowClose;
   }
   tableContent += "</table>\n";
-
   return tableContent;
 }
 function htmlToElement(html) {
@@ -29,132 +30,176 @@ function htmlToElement(html) {
     template.innerHTML = html;
     return template.content.firstChild; //in memory representation of DOM element
 }
-var board1 = htmlToElement(createTenByTenBoard(1));
-var board2 = htmlToElement(createTenByTenBoard(2));
+const tittle1 = "<caption>My Board</caption>\n";
+const tittle2 = "<caption>Opponent's Board</caption>\n";
+var board1 = htmlToElement(createTenByTenBoard(1, tittle1));
+var board2 = htmlToElement(createTenByTenBoard(2, tittle2));
 document.getElementById("board1").appendChild(board1);
 document.getElementById("board2").appendChild(board2);
 const cells = document.querySelectorAll('.cell');
 
-var myShip = {
-  shipFive: {
-    length: 5,
-    location:[] //array of coordinate 5 string
+const templateShip = {
+  Carrier: {
+    length: 5
   },
-  shipFour: {
-    length: 4,
-    location:[] //array of 4 string
+  Battleship: {
+    length: 4
   },
-  shipThree1: {
-    length: 3,
-    location:[]
+  Cruiser: {
+    length: 3
   },
-  shipThree2: {
-    length: 3,
-    location:[]
+  Submarine: {
+    length: 3
   },
-  shipTwo: {
-    length: 2,
-    location:[]
+  Destroyer: {
+    length: 2
   }
 };
 
-var opShip = {
-  shipFive: {
-    length: 5,
-    location:[] //array of coordinate 5 string
-  },
-  shipFour: {
-    length: 4,
-    location:[] //array of 4 string
-  },
-  shipThree1: {
-    length: 3,
-    location:[]
-  },
-  shipThree2: {
-    length: 3,
-    location:[]
-  },
-  shipTwo: {
-    length: 2,
-    location:[]
-  }
-};
-
-const hit = 'H';
-const miss = 'O';
-
-function getRandomInt(max) {
+const getRandomInt = (max) => {
   return Math.floor(Math.random() * max + 1);
 }
 
 function createShip(ship) {
+
+  let listCoordinate = [];
   var HorV = getRandomInt(2); //1->horizontal 2->vertical
   var rowcol = getRandomInt(10);
   if (HorV === 2) { //2->v get col A-F
-    var index = rowcol + 65;
+    var index = rowcol + 64;  //A 65
     rowcol = String.fromCharCode(index);
   }  //now I know which row or col my ship is
   var first = getRandomInt(ship.length+1);
   if (HorV === 1) {
-    var index = 65 + first - 1;
+    var index = 64 + first;
     for (var i = 0; i < ship.length; i++) {
       var col = String.fromCharCode(index + i);
-      var coordinate = rowcol + col;
-      ship.location.push(coordinate);
+      var coordinate = col + rowcol;
+      listCoordinate.push(coordinate);
     }
   } else {
     for (var i = 0; i < ship.length; i++) {
       var row = first + i;
-      var coordinate = row + rowcol;
-      ship.location.push(coordinate);
+      var coordinate = rowcol + row;
+      listCoordinate.push(coordinate);
     }
   }
-  return ship;
+  return listCoordinate;
 }
 
-function overlap(occupied, location) {
-  for (var i in location) {
-    for (var j in occupied) {
-      if (location[i] === occupied[i]) {
-        return false;
-      }
-    }
+function overlap(occupied, listCoordinate) {
+  for (var i in listCoordinate) {
+    if (occupied.includes(listCoordinate[i])) return false;
   }
   return true; //false if overlap
 }
 
-function setup(shipList) {
+function setup(shipList,tableid) {
   var occupied = [];
   for (var ship in shipList) {
     while (true) {
-      var newShip = createShip(shipList[ship]);
-      if (overlap(occupied, shipList[ship].location)) {
+      var listCoordinate = createShip(shipList[ship]);
+      if (overlap(occupied, listCoordinate)) {
         break;
       }
     }
-    occupied.concat(newShip.location);
+    shipList[ship].location = listCoordinate;
+    occupied = occupied.concat(listCoordinate);
+    for (let i in listCoordinate) {
+      var query = "#" + tableid + " #" + listCoordinate[i];
+      document.querySelector(query).style.backgroundColor = "coral";
+    }
   }
+  addStatus(shipList);
   return shipList;
 }
 
-function startGame() {
-  setup(myShip);
-  setup(opShip);
-
-
-  for (var i = 0; i < cells.length; i++) {
-    cells[i].innerText = '';
- //   cells[i].style.removeProperty('background-color');
-    cells[i].addEventListener('click', turnClick, false);
+function addStatus(shipList) {
+  for (let ship in shipList) {
+    let location = shipList[ship].location;
+    shipList[ship].status = {};
+    location.forEach((element) => {
+      shipList[ship].status[element] = false;
+    });
   }
 }
 
-function turnClick(grid) {
-  console.log(grid.target.id);
+const hit = 'H';
+const miss = 'O';
+var myShip = JSON.parse(JSON.stringify(templateShip));
+var opShip = JSON.parse(JSON.stringify(templateShip));
+
+function startGame() {
+  document.querySelector(".endgame").style.display = "none";
+  for (var i = 100; i < cells.length; i++) { //cells.length
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+    cells[i].addEventListener('click', turnClick, false);
+  }
+  for (var i = 0; i < 100; i++) {
+    cells[i].innerText = '';
+    cells[i].style.removeProperty('background-color');
+  }
+  setup(myShip,"table1");
+  setup(opShip,"table2");
 }
 
-function turn(squareId, player) {
-  origBoard[squareId] = player;
-  document.getElementById(squareId).innerText = player;
+function turnClick(grid) {
+  let query1 = '#table2 #' + grid.target.id;
+  turn(grid.target.id, query1, opShip);
+  let row = getRandomInt(10);
+  let col = getRandomInt(10) + 64;
+  col = String.fromCharCode(col);
+  let id = col + row;
+  let query = '#table1 #' + col + row;
+  if (gameOver(myShip, opShip)) turn(id, query, myShip);
 }
+
+function turn(id, query, shipList) {
+  if (hitormiss(id, shipList)) {
+    document.querySelector(query).innerText = hit;
+  } else {
+    document.querySelector(query).innerText = miss;
+  }
+}
+
+function hitormiss(coordinate, shipList) {
+  for (let ship in shipList) {
+    let status = shipList[ship].status;
+    for (let i in status) {
+      if (coordinate === i) {
+        status[i] = true;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function gameOver(my, op) {
+  console.log(ifAllSink(my));
+  if (ifAllSink(my)) {
+    declareWinner("Opponent");
+  } else if (ifAllSink(op)) {
+    declareWinner("You");
+  } else {
+    return true;
+  }
+}
+
+function ifAllSink(shipList) { //true if all sink
+  for (let ship in shipList) {
+    let status = shipList[ship].status;
+    for (let i in status) {
+      if (status[i] === false) return false;
+    }
+  }
+  return true;
+}
+
+function declareWinner(player) {
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = player + " Won";
+}
+
+
